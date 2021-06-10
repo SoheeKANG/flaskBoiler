bcbce35fb48c4652d6057b0821c0a0bd5648b115
soheeKANG <hnb4831@naver.com>
2021-06-10 16:06:52 +0900

    minor update

--- src/database.py
+++ src/database.py
@@ -1,3 +1,6 @@
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -5,9 +8,44 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = ""
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    sqlalchemy_url = 'dummy'
+    engine = create_engine(sqlalchemy_url)
+
+    if 'conn' not in g:
+        g.conn = engine.connect()
+
+    return g.conn
+
+
+def close_db(e=None):
+    conn = g.pop('conn', None)
+
+    if conn is not None:
+        conn.close()
+
+
+def init_db():
+    db = get_db()
+    with current_app.open_resource("schema.sql") as f:
+        for item in f.read().decode("utf-8").split(';'):
+            print(item)
+            db.execute(item)
+
+    click.echo("Initialized the database.")
+
+
+@click.command("init_db")
+@with_appcontext
+def init_db_command():
+    init_db()
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
--- src/models.py
+++ src/models.py
@@ -1,8 +1,7 @@
-from sqlalchemy import Column, Integer, String, Text
-from .database import Base
+from sqlalchemy import Column, Integer, String, Text, TIMESTAMP, ForeignKey
 
 
-class User(Base):
+class User:
     __tablename__ = "User"
 
     id = Column(String(20), primary_key=True)
@@ -11,9 +10,22 @@ class User(Base):
     email = Column(String(30), unique=True, nullable=False)
 
 
-class Post(Base):
+class Post:
     __tablename__ = "Post"
 
-    postNumber = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(Text, nullable=False)
-    body = Column(Text, nullable=False)
+    post_number = Column(Integer, primary_key=True, autoincrement=True)
+    post_title = Column(Text, nullable=False)
+    post_body = Column(Text, nullable=False)
+    post_time = Column(TIMESTAMP)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+
+class Comment:
+    __tablename__ = "comment"
+    post_num = Column(Integer)
+    comment_num = Column(Integer, autoincrement=True)
+    comment_body = Column(Text)
+    comment_time = Column(TIMESTAMP)
